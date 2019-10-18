1eaccda5b54f493024dfdb3342c30ab823847334
Pavlo Dzhyvak <pav8802@gmail.com>
2019-10-18 20:46:58 +0300

    fix hw js1

--- hw1js/script.js
+++ hw1js/script.js
@@ -3,20 +3,21 @@ let age="";
 // name=prompt("Enter your name");
 // age= +prompt("Enter your age");
 let allowed;
-let notAllowed = "You are not allowed to visit this website";
+// let notAllowed = "You are not allowed to visit this website";
 do{
     name=prompt('Enter your name', name);
     age= +prompt("Enter your age",age)
 }
-while (name==="" || isNaN(age)===true||age<=0);
+// while (name==="" || isNaN(age)===true||age<=0);
+while (name==="" || isNaN(age)||age<=0);
 allowed ="Welcome "+name;
 if(age <18){
-    alert(notAllowed)
+    alert("You are not allowed to visit this website")
 }else if(age<=22 ){
-    let wantToContinue = confirm("Are you sure you want to continue?");
-    if(wantToContinue===true){
+    // let wantToContinue = confirm("Are you sure you want to continue?");
+    if(confirm("Are you sure you want to continue?")){
 alert(allowed)
     }else
-        alert(notAllowed)
+        alert("You are not allowed to visit this website")
 }
 else alert(allowed);
---
+++ hw3js/script.js
@@ -0,0 +1,39 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function calc(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return num1 + +num2;
+            break;
+        case "-":
+            return num1 - num2;
+            break;
+        case "*":
+            return num1 * num2;
+            break;
+        case "/":
+            return num1 / num2;
+            break;
+
+    }
+}
+
+do {
+    num1 = +prompt("Enter number 1", num1);
+    operator = prompt("Enter operator", operator);
+    num2 = +prompt("Enter number 2", num2);
+    console.log(isNaN(num1));
+    console.log(isNaN(num2));
+    console.log(operator !== "+"&& operator !== "-" && operator !== "/" && operator !== "*");
+    // console.log(operator !== "/");
+    // console.log(operator !== "*");
+    // console.log(operator !== "-");
+}
+
+while (isNaN(num1) === true || isNaN(num2) === true ||
+(operator !== "+"&& operator !== "-" && operator !== "/" && operator !== "*"));
+
+
+console.log(calc(num1, num2, operator));
