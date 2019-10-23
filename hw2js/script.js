f819377ea77940fbfe374374bf1c92cc9df8b236
Pavlo Dzhyvak <pav8802@gmail.com>
2019-10-23 19:02:34 +0300

    hw js2

--- hw2js/script.js
+++ hw2js/script.js
@@ -1,53 +1,53 @@
-// let num = 0;
-// let result = "";
-// // let noNumb = "Sorry, no numbers.";
+let num = 0;
+let result = "";
+// let noNumb = "Sorry, no numbers.";
+
+do {
+    num = +prompt("Enter integer, positive number");
+}
+while (isNaN(num) === true || num < 0 || num % 1 !== 0);
+
+if (num < 5) {
+    result = "Sorry, no numbers.";
+} else
+    result = "Multiples five: ";
+for (let i = 5; i <= num; i++) {
+    let resDivision = i % 5;
+    if (resDivision === 0) {
+        result += i+", ";
+        console.log(result)
+    }
+}
+
+alert(result);
+
+// закоментуйте все що вище і розкоментуйте все, що нижче
+// let m;
+// let n;
 //
-// do {
-//     num = +prompt("Enter integer, positive number");
-//    }
-// while (isNaN(num) === true || num < 0 || num % 1 !== 0);
+// m = +prompt("Enter integer, positive number (start of range)");
+// n = +prompt("Enter integer, positive number (end of range)");
 //
-// if (num < 5) {
-//     result = "Sorry, no numbers.";
-// } else
-//     result = "Multiples five: ";
-// for (let i = 5; i <= num; i++) {
-//     let resDivision = i % 5;
-//     if (resDivision === 0) {
-//         result += i+", ";
-//         console.log(result)
-//     }
+// while (isNaN(n) === true || n < 0 || n % 1 !== 0 || isNaN(m) === true || m < 0 || m % 1 !== 0 || m > n) {
+//     alert("Error");
+//     m = +prompt("Enter integer, positive number (start of range)");
+//     n = +prompt("Enter integer, positive number (end of range)");
 // }
 //
+// let result = "Prime number(s): ";
+// let count = 0;
+// for (let i = m-1; i <= n; i++) {
+//     for (let k = 1; k <= i; k++) {
+//         let resDivision = i % k;
+//         // let count = 0;
+//         if (resDivision === 0) {
+//             ++count;
+//         }
+//     }
+//
+//     if (count === 2) {
+//         result += i+", ";
+//     }
+//     count=0;
+// }
 // alert(result);
-
-
-let m;
-let n;
-
-m = +prompt("Enter integer, positive number (start of range)");
-n = +prompt("Enter integer, positive number (end of range)");
-
-while (isNaN(n) === true || n < 0 || n % 1 !== 0 || isNaN(m) === true || m < 0 || m % 1 !== 0 || m > n) {
-    alert("Error");
-    m = +prompt("Enter integer, positive number (start of range)");
-    n = +prompt("Enter integer, positive number (end of range)");
-}
-
-let result = "Prime number(s): ";
-let count = 0;
-for (let i = m-1; i <= n; i++) {
-    for (let k = 1; k <= i; k++) {
-        let resDivision = i % k;
-        // let count = 0;
-        if (resDivision === 0) {
-            ++count;
-        }
-    }
-
-    if (count === 2) {
-        result += i+", ";
-    }
-    count=0;
-}
-alert(result);
