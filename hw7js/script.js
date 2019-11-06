c804634606b4025bd0c5df143dd3aac8cb57e376
Pavlo Dzhyvak <pav8802@gmail.com>
2019-11-06 13:04:00 +0200

    hw7js

---
+++ hw7js/script.js
@@ -0,0 +1,19 @@
+let arr = ['hello', 'world', 'Kiev', 'Kharkiv', 'Odessa', 'Lviv'];
+let arr2 = ['1', '2', '3', 'sea', 'user', 23];
+
+function createList(array){
+    let list = document.createElement('ul');
+    document.body.prepend(list);
+    array.map((elem, i)=>{
+        let item= document.createElement('li');
+        item.innerHTML = `елемент ${i+1} - '${elem}'`;
+        list.appendChild(item);
+    })
+}
+
+
+
+// let item= document.createElement('li');
+// list.append(item);
+// list.prepend(item);
+// list.append(item);
