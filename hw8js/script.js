51dfac9766b726789636832a00c6a5ddd79eb6f6
Pavlo Dzhyvak <pav8802@gmail.com>
2019-11-10 11:23:51 +0200

    hw8js,delete one listener, fix function changeStyleBlur

--- hw8js/script.js
+++ hw8js/script.js
@@ -1,54 +1,27 @@
 let input = document.querySelector('.inputPrice');
 
-// input.addEventListener('blur', changeStyleBlur);
 input.addEventListener('blur', addSpanWithPrice);
 input.addEventListener('focus', changeStyleFocus);
-
-//
-// let spanWithPrice = document.createElement('span');
-// let closeSpan = document.createElement('span');
-
-// function operateWithInput(changeStyleBlur,) {
-//     changeStyleBlur();
-//
-// }
-
-function changeStyleBlur() {
-// let value = +this.value;
-//     console.log(this.value);
-    if (this.value < 0 || isNaN(this.value)) {
-        this.style.borderColor = 'rgba(203, 32, 37, 0.89)';
-    } else {
-        this.style.color = 'rgba(23, 203, 14, 0.89)';
-        this.style.borderColor = '';
-    }
-}
 
 function changeStyleFocus() {
     this.style.color = 'rgba(0, 0, 0,1)';
     this.style.outlineColor = 'rgba(23, 203, 14, 0.89)';
-
     if (this.parentElement.nextElementSibling) {
         this.parentElement.nextElementSibling.remove()
-    }
-    else if (this.parentElement.previousElementSibling){
+    } else if (this.parentElement.previousElementSibling) {
         this.parentElement.previousElementSibling.remove()
     }
 
 }
 
 function addSpanWithPrice() {
-
     let thiss = this;
-    changeStyleBlur();
+    changeStyleBlur(thiss);
     if (this.value < 0 || isNaN(this.value)) {
-
         let spanWithError = document.createElement('span');
         spanWithError.innerHTML = `Please enter correct price`;
         thiss.parentElement.after(spanWithError);
-
     } else {
-
         let spanWithPrice = document.createElement('span');
         spanWithPrice.innerHTML = `Current price ${thiss.value}`;
         spanWithPrice.classList.add('spanWithPrice');
@@ -68,6 +41,12 @@ function addSpanWithPrice() {
     }
 }
 
-
-
-
+function changeStyleBlur(target) {
+    console.log(target.value);
+    if (target.value < 0 || isNaN(target.value)) {
+        target.style.borderColor = 'rgba(203, 32, 37, 0.89)';
+    } else {
+        target.style.color = 'rgba(23, 203, 14, 0.89)';
+        target.style.borderColor = '';
+    }
+}
